86e1f4e942a5232e97959214d28dfcaab4a2ab6a
no7dw <no7david@gmail.com>
2017-07-08 12:27:48 +0800

    disable router

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+  {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      message: 'AWelcome to Your App'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 260px;
+}
+</style>
